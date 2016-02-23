ccf633bda33d734f64c825d5adc5eb03171415a3
Jayce Miller <jaycem@smu.edu>
2016-02-23 17:56:56 -0600

    can parse json. need to make results controller

--- beerMe/www/js/myAngular.js
+++ beerMe/www/js/myAngular.js
@@ -1,6 +1,6 @@
 var app = angular.module('ionicApp', ['ionic']);
 
-app.controller('AppCtrl', function($scope) {
+app.controller('AppCtrl', function($scope, $http, $state) {
   $scope.search = {searchInput: ""};
 
   /*$scope.master = {};
@@ -12,6 +12,20 @@ app.controller('AppCtrl', function($scope) {
   $scope.clearInput = function() {
     console.log($scope.search.searchInput);
     $scope.search.searchInput = "";
-
   };
+
+  $scope.searchBeer = function() {
+    console.log($scope.search.searchInput);
+    $http({
+      method: 'GET',
+      url: 'https://salty-taiga-88147.herokuapp.com/search?q=' + $scope.search.searchInput +'&type=beer'
+    }).then(function successfulCallback(response) {
+      //puts data objects containing name and description into array
+      $scope.myArray = response.data.data;
+      $state.go('app.results');
+    }, function errorCallback(response) {
+      console.log('uh oh');
+    });
+  };
+
 });
